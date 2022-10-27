0ff29516fdd6629ed7c59a885e3471c14f8aa444
yusooo <yusaei1107@gmail.com>
2022-10-27 20:24:23 +0900

    데이터타입 수정 / 토큰 추가

--- src/routes/todo.js
+++ src/routes/todo.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express();
 
+const AuthMiddleware = require("../middleware/token");
 const controller = require('../controllers/todo');
 
-router.post('/', controller.writeTodo);
+router.post('/', AuthMiddleware, controller.writeTodo);
 
 module.exports = router;
