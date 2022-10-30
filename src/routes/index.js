98713e1788156076fe7f878fd4e1cfe76121a9a1
soyeonkim0227 <soyeonkim0227@naver.com>
2022-10-30 14:52:48 +0900

    Update node index

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,8 +3,10 @@ const router = express();
 
 const User = require("./user");
 const Todo = require('./todo');
+const Comment = require("./comment");
 
 router.use("/user", User);
 router.use("/todo", Todo);
+router.use("/todo", Comment);
 
 module.exports = router;
