2857bcd160e4b24c573a5b423bfb1001b97de7fc
yuso <yusaei1107@dsm.hs.kr>
2022-11-01 20:19:43 +0900

    Create node updateTodo api

--- src/controllers/todo.js
+++ src/controllers/todo.js
@@ -1,4 +1,5 @@
 const { Todo } = require('../models');
+const { User } = require('../models');
 
 const createTodo = async(req, res) => {
     const { sub, content, check } = req.body;
@@ -6,10 +7,10 @@ const createTodo = async(req, res) => {
 
     try{
         await Todo.create({
+            writer : user.id,
             sub,
             content,
-            check,
-            writer : user.id
+            check
         });
 
         return res.status(201).json({
@@ -23,6 +24,77 @@ const createTodo = async(req, res) => {
     }
 };
 
+
+const updateTodo = async(req, res) => {
+    const todoId = req.params.todo_id;
+    const UserId = req.decoded.id;
+    const { sub, content, check } = req.body;
+
+    try{
+        const todo = await Todo.findOne({
+            where : { id : todoId },
+        })
+        if(!todoId){
+            res.status(404).json({
+                "message" : "존재하지 않는 투두입니다."
+            });
+            console.error(err);
+        }else if(todo.writer !== UserId){
+            res.status(403).json({
+                "message" : "투두는 해당 투두의 작성자만 수정할 수 있습니다."
+            })
+        }else{
+            await todo.update({
+                sub,
+                content,
+                check,
+            });
+            res.status(200).json({
+                "message" : "투두가 수정되었습니다."
+            });
+        }
+    }catch(err){
+        res.status(400).json({
+            "message" : "잘못된 요청입니다."
+        });
+        console.error(err);
+    };
+}
+
+
+// const deleteTodo = async(req, res) => {
+//     const todoId = req.params.todo_id;
+//     const user = req.decoded.user_id;
+
+//     try{
+//         if(User.id !== user){
+//             res.status(403).json({
+//                 "message" : "해당 투두의 작성자만 삭제 가능합니다." 
+//             });
+//             console.error(err);
+//         }else if(Todo.id != todoId){
+//             console.error(err);
+//             res.status(404).json({
+//                 "message" : "존재하지 않는 투두입니다."
+//             });
+//         }else{
+//             /*
+//             await Like.destroy({
+//                 where : { todo_id : todoId },
+//             });*/
+//             await Todo.destroy({})
+//             res.status(204).json({})
+//         }
+//     }catch(err){
+//         console.error(err);
+//         res.status(400).json({
+//             "message" : "잘못된 요청입니다."
+//         })
+//     }
+// }
+
 module.exports = {
-    createTodo
+    createTodo,
+    updateTodo,
+    //deleteTodo,
 };
