478a2b807e09b8e69a4588d4c140a971b7c48617
yuso <yusaei1107@dsm.hs.kr>
2022-11-01 21:12:17 +0900

    Create node updateTodo api

--- src/controllers/todo.js
+++ src/controllers/todo.js
@@ -1,4 +1,5 @@
 const { Todo } = require('../models');
+const { User } = require('../models');
 
 const createTodo = async(req, res) => {
     const { sub, content, check } = req.body;
@@ -6,10 +7,10 @@ const createTodo = async(req, res) => {
 
     try{
         await Todo.create({
+            writer : user.id,
             sub,
             content,
-            check,
-            writer : user.id
+            check
         });
 
         return res.status(201).json({
@@ -23,6 +24,43 @@ const createTodo = async(req, res) => {
     }
 };
 
+const updateTodo = async(req, res) => {
+    const todoId = req.params.todo_id;
+    const UserId = req.decoded.id;
+    const { sub, content, check } = req.body;
+
+    try{
+        const todo = await Todo.findOne({
+            where : { id : todoId },
+        })
+        if(!todoId){
+            res.status(404).json({
+                "message" : "존재하지 않는 투두입니다."
+            });
+            console.error(err);
+        }else if(todo.writer !== UserId){
+            res.status(403).json({
+                "message" : "투두는 해당 투두의 작성자만 수정할 수 있습니다."
+            })
+        }else{
+            await todo.update({
+                sub,
+                content,
+                check,
+            });
+            res.status(200).json({
+                "message" : "투두가 수정되었습니다."
+            });
+        }
+    }catch(err){
+        res.status(400).json({
+            "message" : "잘못된 요청입니다."
+        });
+        console.error(err);
+    };
+}
+
 module.exports = {
-    createTodo
+    createTodo,
+    updateTodo,
 };
--- src/routes/todo.js
+++ src/routes/todo.js
@@ -5,5 +5,6 @@ const AuthMiddleware = require("../middleware/token");
 const controller = require('../controllers/todo');
 
 router.post('/', AuthMiddleware, controller.createTodo);
+router.patch('/:todo_id', AuthMiddleware, controller.updateTodo);
 
 module.exports = router;
