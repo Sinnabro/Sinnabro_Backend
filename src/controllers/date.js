cc17850774b5b93c88c88a854ed12fd8a476c18b
soyeonkim0227 <soyeonkim0227@naver.com>
2022-11-05 23:24:48 +0900

    Create node delete date api

--- src/controllers/date.js
+++ src/controllers/date.js
@@ -42,6 +42,44 @@ const createDate = async(req, res) => {
     }
 };
 
+const deleteDate = async(req, res) => {
+    const DateId = req.params.id;
+    const UserId = req.decoded.id;
+
+    try {
+        const date = await Date.findOne({
+            where: {
+                id: DateId
+            }
+        });
+
+        
+        if (!date) {
+            return res.status(404).json({
+                message: "존재하지 않는 디데이입니다."
+            });
+        } else if ( date.user_id !== UserId) {
+            return res.status(403).json({
+                message: "디데이는 본인만 삭제할 수 있습니다."
+            });
+        } else {
+            await date.destroy();
+
+            return res.status(200).json({
+                message: "디데이가 삭제되었습니다."
+            });
+        }
+
+    } catch(err) {
+        console.error(err);
+
+        return res.status(400).json({
+            message: "잘못된 요청입니다."
+        });
+    }
+};
+
 module.exports = {
-    createDate
+    createDate,
+    deleteDate
 };
