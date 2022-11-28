d6c19736987647531e2eaf743994b4fe35b39276
yuso <yusaei1107@dsm.hs.kr>
2022-11-28 09:36:55 +0900

    Create node read api

--- src/controllers/time.js
+++ src/controllers/time.js
@@ -33,7 +33,34 @@ const createTime = async(req, res) => {
         });
     }
 };
+const getTime = async(req, res) => {
+    const user = req.decoded.id;
+    
+    try{
+        const times = await Time.findAll({
+            where: {
+                user_id: user,
+                date: moment().format('YYYY-MM-DD'),
+            }
+        })
+        if(!times){
+            return res.status(404).json({
+                "message" : "타임테이블이 존재하지 않습니다."
+            })
+        }
+        res.status(200).json({
+            "message" : "공부 시간을 조회합니다.",
+            times,
+        })
+    }catch(err){
+        console.error(err)
+        res.status(400).json({
+            "message" : "잘못된 요청입니다.",
+        });
+    }
+};
 
 module.exports = {
     createTime,
+    getTime,
 };
--- src/routes/time.js
+++ src/routes/time.js
@@ -5,5 +5,6 @@ const AuthMiddleware = require("../middleware/token");
 const controller = require('../controllers/time');
 
 router.post('/', AuthMiddleware, controller.createTime);
+router.get('/', AuthMiddleware, controller.getTime);
 
 module.exports = router;
