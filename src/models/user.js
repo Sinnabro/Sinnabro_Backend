d7173cd574db1dee4f3fd2667f4ee6642d5edf0d
soyeonkim0227 <soyeonkim0227@naver.com>
2022-12-02 20:21:43 +0900

    Create node verify code api

--- src/models/user.js
+++ src/models/user.js
@@ -1,30 +1,34 @@
 const sequelize = require("sequelize");
 
 module.exports = (sequelize, DataTypes) => {
-    return sequelize.define("user", {
-        id: {
-            type: DataTypes.INTEGER,
-            primaryKey: true,
-            allowNull: false,
-            autoIncrement: true
-        },
-        email: {
-            type: DataTypes.STRING(30),
-            allowNull: false,
-            unique: true
-        },
-        name: {
-            type: DataTypes.STRING(10),
-            allowNull: false,
-            unique: true
-        },
-        password: {
-            type: DataTypes.STRING(),
-            allowNull: false
-        },
-        token: {
-            type: DataTypes.STRING(),
-            allowNull: true
-        }
-    });
+  return sequelize.define("user", {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true,
+    },
+    email: {
+      type: DataTypes.STRING(30),
+      allowNull: false,
+      unique: true,
+    },
+    name: {
+      type: DataTypes.STRING(10),
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING(),
+      allowNull: false,
+    },
+    token: {
+      type: DataTypes.STRING(),
+      allowNull: true,
+    },
+    verify_code: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+  });
 };
