a5cedc634f2d0e66bb65e1933c59acf6e5790d62
soyeonkim0227 <soyeonkim0227@naver.com>
2023-01-14 13:33:59 +0900

    Update node like api

--- src/controllers/like.js
+++ src/controllers/like.js
@@ -1,69 +1,71 @@
-const { Like, Time } = require('../models');
-const { QueryTypes } = require('sequelize');
-const { sequelize } = require('../models/index');
-const { createLike, deleteLike } = require('../middleware/like');
+const { Like, Time } = require("../models");
+const { createLike, deleteLike } = require("../middleware/like");
 
-const getLike = async(req, res) => {
-    const { timeId } = req.params;
-    const user = req.decoded.id;
-    try{
-        const like = await Like.findOne({
-            where:{
-                time_id: timeId,
-            }
-        });
-        if(!like) return res.status(404).json({
-            "message" : "좋아요가 존재하지 않습니다.",
-        })
-        const count = await Like.count({
-            where : {
-                time_id: timeId
-            }
-        })
-        return res.status(200).json({
-            "message" : "좋아요 수를 조회합니다.",
-            count,
-        })
-    }catch(error){
-        console.log(error);
-        return res.status(400).json({
-            "message" : "잘못된 요청입니다.",
-        })
-    }
-}
+const getLike = async (req, res) => {
+  const { timeId } = req.params;
+  const user = req.decoded.id;
+  try {
+    const like = await Like.findOne({
+      where: {
+        time_id: timeId,
+        user_id: user,
+      },
+    });
+    if (!like)
+      return res.status(404).json({
+        message: "좋아요가 존재하지 않습니다.",
+      });
+    const count = await Like.count({
+      where: {
+        time_id: timeId,
+      },
+    });
+    return res.status(200).json({
+      message: "좋아요 수를 조회합니다.",
+      count,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({
+      message: "잘못된 요청입니다.",
+    });
+  }
+};
 
-const CDLike = async(req, res) => {
-    const { timeId } = req.params;
-    const user = req.decoded.id;
-    try{        
-        if(!timeId) return res.status(404).json({
-            "message" : "존재하지 않는 타임테이블입니다.",
-        })
-        const having = await Like.findOne({
-            where: {
-                time_id : timeId,
-                user_id : user,
-            }
-        })
-        if(having){
-            deleteLike(req, res);
-            return res.status(204).json({
-                "message" : "좋아요를 취소했습니다.",
-            })
-        }
-        else createLike(req, res);
-        return res.status(200).json({
-            "message" : "좋아요를 생성했습니다."
-        })
-    }catch(err){
-        console.error(err);
-        return res.status(400).json({
-            "message" : "잘못된 요청입니다.",
-        })
+const CDLike = async (req, res) => {
+  const { timeId } = req.params;
+  const user = req.decoded.id;
+  try {
+    if (!timeId)
+      return res.status(404).json({
+        message: "존재하지 않는 타임테이블입니다.",
+      });
+    const having = await Like.findOne({
+      where: {
+        time_id: timeId,
+        user_id: user,
+      },
+    });
+    if (having) {
+      deleteLike(req, res);
+      return res.status(200).json({
+        message: "좋아요를 취소했습니다.",
+      });
+    } else {
+      createLike(req, res);
+      return res.status(200).json({
+        message: "좋아요를 생성했습니다.",
+      });
     }
-}
+  } catch (err) {
+    console.error(err);
+    return res.status(400).json({
+      message: "잘못된 요청입니다.",
+    });
+  }
+};
 
 module.exports = {
-    getLike,
-    CDLike,
+  getLike,
+  CDLike,
 };
--- src/models/like.js
+++ src/models/like.js
@@ -1,24 +1,31 @@
 const sequelize = require("sequelize");
 
 module.exports = (sequelize, DataTypes) => {
-    return sequelize.define("like", {
-        time_id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true,
-        },
-        user_id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-        },
+  return sequelize.define(
+    "like",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      time_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
     },
     {
-        sequelize,
-        timestamps: false,
-        modelName: 'Like',
-        tableName: 'likes',
-        charset: 'utf8mb4',
-        collate: 'utf8mb4_general_ci',
-    });
+      sequelize,
+      timestamps: false,
+      modelName: "Like",
+      tableName: "likes",
+      charset: "utf8mb4",
+      collate: "utf8mb4_general_ci",
+    }
+  );
 };
